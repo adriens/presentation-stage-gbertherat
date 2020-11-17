477e47ac689a901b4b9b2611668f03e9b69f2c14
Bertherat Guillaume <bertherat.guillaume@gmail.com>
2020-11-17 16:26:19 +1100

    Added background canvas

--- resources/js/sketch.js
+++ resources/js/sketch.js
@@ -1,5 +1,5 @@
 let yoffset = 0;
-let step = 20;
+let step = 60;
 
 let xconstraint = 40;
 let yconstraint = 40;
@@ -10,30 +10,40 @@ function setup(){
     canvas.parent("canvas");
     canvas.id("sketch");
 
-    noFill();
-    colorMode(HSB, 255); 
-    blendMode(ADD);
     background(30);
 }
 
 function draw(){
-    if(yoffset > 255){
+    if(yoffset*50 > 255){
         yoffset = 0;
     }
-    stroke(yoffset*50, 255, 10, 100);
+    blendMode(NORMAL);
+    colorMode(RGB);
+    fill(30);
+    noStroke();
+    rect(width-xconstraint, 0, width, height);
+
+    blendMode(ADD);
+    colorMode(HSB, 255); 
+    noFill();
+    stroke(yoffset*50, 255, 20, 100);
     strokeWeight(1);
     let xoffset = 0;
     beginShape();
-    for(let x = xconstraint; x < width + step - xconstraint; x = x + step){
-        let y = map(noise(xoffset, yoffset), 0.4, 0.6, yconstraint, height - yconstraint, true);
+    for(let x = xconstraint; x < width-xconstraint/2+step/2; x = x + step){
+        let rng = random(0,0.1);
+        let y = map(noise(xoffset, yoffset)+rng, 0.3, 0.6, yconstraint, height - yconstraint, true);
         vertex(x, y);
         xoffset += 0.01;
     }
-    yoffset += 0.002;
     endShape();
+
+    yoffset += 0.003;
     stroke(0, 0, 255);
     strokeWeight(10);
     rect(xconstraint, yconstraint, width-xconstraint*2, height - yconstraint*2);
+
+    
 }
 
 function windowResized(){
